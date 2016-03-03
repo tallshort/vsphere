19b7dc4ad33f5dbe9946422eda95d24209ba611e
Jian Zhang <jzhang@splunk.com>
2016-03-03 16:57:01 +0800

    add confirmed param

--- pyvmomi-master/vshpereAuto/vsphere_helper.py
+++ pyvmomi-master/vshpereAuto/vsphere_helper.py
@@ -58,14 +58,15 @@ def power_off(vmFolderList, vappname):
             break
 
 
-def start_operation(operation, vappname, snapname):
-    raw_input('Press ENTER to confirm vappname: {v}'.format(v=vappname))
-    if bool(snapname):
-        raw_input('Press ENTER to confirm operation: {o} {s}'.format(o=operation, s=snapname))
-        
-    else:
-        raw_input('Press ENTER to confirm operation: {o}'.format(o=operation))
-    raw_input('Press ENTER to start ...')
+def start_operation(operation, vappname, snapname, arguments_confirmed=False):
+    if not arguments_confirmed:
+        raw_input('Press ENTER to confirm vappname: {v}'.format(v=vappname))
+        if bool(snapname):
+            raw_input('Press ENTER to confirm operation: {o} {s}'.format(o=operation, s=snapname))
+            
+        else:
+            raw_input('Press ENTER to confirm operation: {o}'.format(o=operation))
+        raw_input('Press ENTER to start ...')
 
     with open("params.yml", 'r') as stream:
         params = yaml.load(stream)
@@ -118,7 +119,12 @@ if __name__ == '__main__':
         '-a', '--vappname', default=None, help='vpp name')
     parser.add_argument(
         '-s', '--snapname', default=None, help='snapshot name')
+    parser.add_argument(
+        '--confirmed',
+        help='confirm the arguments without interaction',
+        action='store_true')
 
     args = parser.parse_args()
     start_operation(
-        operation=args.operation, vappname=args.vappname, snapname=args.snapname)
+        operation=args.operation, vappname=args.vappname, snapname=args.snapname, 
+        arguments_confirmed=args.confirmed)
